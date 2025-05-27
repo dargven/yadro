22f4558c7aaba56e9bfda0126fdaabb8bf862dfa
dargven <42145310+dargven@users.noreply.github.com>
2025-05-27 22:00:05 +0400

    add alembic

--- app/database.py
+++ app/database.py
@@ -5,9 +5,10 @@ from sqlalchemy import func
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncAttrs
 from sqlalchemy.orm import DeclarativeBase, declared_attr, Mapped, mapped_column
 
-from app.config import get_db_url
+from app.config import get_db_url, get_sync_db_url
 
 DATABASE_URL = get_db_url()
+SYNC_DATABASE_URL = get_sync_db_url()
 
 engine = create_async_engine(DATABASE_URL)
 async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
